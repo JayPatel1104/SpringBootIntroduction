89da0d2d7cde358263e94a6e5e5df25c270be0d2
Jay Patel <jupatel1104@gmail.com>
2024-10-29 13:06:58 -0400

    first bean created and executed

---
+++ src/main/java/com/springBootLearning/jayPatel/week1Introduction/SpringBootIntroduction/Apple.java
@@ -0,0 +1,10 @@
+package com.springBootLearning.jayPatel.week1Introduction.SpringBootIntroduction;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+    public void eatApple() {
+        System.out.println("Eating Apple");
+    }
+}
