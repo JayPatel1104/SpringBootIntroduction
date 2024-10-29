c00e946eac9a7b1f59d37507018b71ad1ca86e79
Jay Patel <jupatel1104@gmail.com>
2024-10-29 12:47:22 -0400

    inital commit

---
+++ src/main/java/com/springBootLearning/jayPatel/week1Introduction/SpringBootIntroduction/SpringBootIntroductionApplication.java
@@ -0,0 +1,14 @@
+package com.springBootLearning.jayPatel.week1Introduction.SpringBootIntroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntroductionApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/springBootLearning/jayPatel/week1Introduction/SpringBootIntroduction/SpringBootIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootLearning.jayPatel.week1Introduction.SpringBootIntroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
